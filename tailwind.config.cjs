4fe9411496247bbf14b4522a5ba177b9eae13671
Anish-Shobith <anish.shobith19@gmail.com>
2023-11-19 21:56:45 +0530

    section: client
    Signed-off-by: Anish-Shobith <anish.shobith19@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 50s linear infinite"
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: 'translateX(0)',
+          },
+          to: {
+            transform: 'translateX(calc(-100% - 2.5rem))',
+          },
+        },
+      },
       backgroundImage: {
         topography: "url('/topography.svg')",
       },
