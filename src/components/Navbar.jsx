d436ad8e02c28092cd19647bc2c4c6db232b53ae
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-11-19 12:31:02 +0530

    fix: Contact form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,11 +25,11 @@ export const Navbar = () => {
       <div className="2xl:w-[1380px] xl:w-10/12 w-11/12 flex justify-between items-center relative">
         <a href="#home" aria-label="Home" className="flex items-center">
           <img
-            className="logo w-16 h-16 invert z-30"
-            src="/logo.png"
+            className="logo w-[3rem] h-[3rem] md:w-16 md:h-16 z-30 hover:rotate-[360deg] duration-1000"
+            src="/logo.webp"
             alt="Fade Media Logo"
           />
-          <span className="text-3xl absolute opacity-0 text-zinc-200 font-bold">
+          <span className="text-3xl absolute left-5 opacity-0 text-zinc-200 font-bold">
             Fade Media
           </span>
         </a>
@@ -39,9 +39,9 @@ export const Navbar = () => {
           transition={{ duration: 0.3 }}
           exit={{ opacity: 0 }}
         >
-          <div className="grow basis-0 justify-end lg:flex">
+          <div className="grow basis-0 justify-end items-center flex">
             <a
-              className="custom-button-colored sm:w-40 h-12 mr-0 sm:mr-4 lg:mr-4 mb-2 sm:mb-0"
+              className="custom-button-colored w-28 sm:w-40 h-8 sm:h-10 mr-0 sm:mr-4 lg:mr-4 sm:mb-0"
               href="#contact"
             >
               Contact Us
